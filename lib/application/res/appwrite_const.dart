e63fce202ca64ff1cfc373fd94b7b186cb600f45
Souvik Biswas <sbis1999@gmail.com>
2022-05-29 22:12:17 +0530

    Add new production consts

--- lib/application/res/appwrite_const.dart
+++ lib/application/res/appwrite_const.dart
@@ -1,14 +1,16 @@
-// PRODUCTION
-// const storiesCollectionId = '627e0433533e8e526c8a';
-// const chaptersCollectionId = '627e055a4b5c9e449c60';
-// const filesBucketId = '627e05f292f31aded78a';
-
 // LOCAL
-const storiesCollectionId = '627a37fd6c3524920a69';
-const purchasedCollectionId = '62892752d95ec1e2da50';
-const chaptersCollectionId = '627be90a0265ba0dc2ae';
-const filesBucketId = '627a52c3c13dedb31cbb';
-const coversBucketId = '62879517575fa2f84650';
+// const storiesCollectionId = '627a37fd6c3524920a69';
+// const purchasedCollectionId = '62892752d95ec1e2da50';
+// const chaptersCollectionId = '627be90a0265ba0dc2ae';
+// const filesBucketId = '627a52c3c13dedb31cbb';
+// const coversBucketId = '62879517575fa2f84650';
+
+// PRODUCTION
+const storiesCollectionId = '62939b4c356735dbd292';
+const purchasedCollectionId = '62939cf8bb03a32892b1';
+const chaptersCollectionId = '62939c7e2fa4a8dc8bc3';
+const filesBucketId = '62939db81e26519fa400';
+const coversBucketId = '62939d8abf3be11efa3a';
 
 const authEmailSharedPrefKey = 'auth_email';
 const authPasswordSharedPrefKey = 'auth_password';
